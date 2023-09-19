9508ba83323db2f25bef0041b773a04155496708
anushsk030 <145442617+anushsk030@users.noreply.github.com>
2023-09-19 15:38:41 +0530

    Add files via upload

---
+++ Java lab/HelloJava.java
@@ -0,0 +1,15 @@
+/*
+ * File         :  HelloJava.java
+ * Description  :  My first java program the "HelloJava"
+ * Author       :  Anush S Kumar 
+ * Version      :  1.0
+ * Date         :19/09/2023   
+*/
+public class HelloJava {
+	public static void  main(String [] args) {
+	   System.out.println("HelloJava");
+	   System.out.print("Java");
+	
+	}
+
+}
